f4ab17231b68bb81cf3058f76faba49a02fb37fd
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-07-21 20:44:40 +0300

    checking total leaves and full yeaR service

--- leaveBalance.js
+++ leaveBalance.js
@@ -4,10 +4,19 @@ const initialLeaveEntitlement = 16;
 function calculateLeaveBalance(joiningDate, leaveStartDate){
     const currentDate = new Date(leaveStartDate);
     const leaveYearStart = new Date(currentDate.getFullYear(),0,1);
-    const fullYear= (360*24*60*60*1000);
+    const fullYear= (365*24*60*60*1000);
      // Calculate the number of full years of service
   const fullYearsOfService = currentDate.getFullYear() - joiningDate.getFullYear();
 // calculating the total number of leaves day for one year or full year of service
   const totalLeaves = initialLeaveEntitlement + fullYearsOfService;
+  
+ // Calculate the leave balance
+ const leaveBalance = Math.floor(totalLeaves);
 
+ return leaveBalance;
 }
+
+const employeeJoiningDate = new Date('2021-05-21'); // Employee joined on May 21, 2021
+const requestedLeaveStartDate = new Date('2023-07-24'); // Requested leave start date: July 24, 2023
+const leaveBalance = calculateLeaveBalance(employeeJoiningDate, requestedLeaveStartDate);
+console.log('Leave Balance:', leaveBalance); 
